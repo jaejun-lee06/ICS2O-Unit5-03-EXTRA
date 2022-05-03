244abec3d8634e635e7d47937a134f9b89023ffb
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-05-03 17:14:20 +0000

    done

--- main.cs
+++ main.cs
@@ -1,7 +1,35 @@
+// Created by: Jaejun Lee
+// Created on: May 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int userInteger;
+        Console.WriteLine("Put your age:");
+        userInteger = int.Parse(Console.ReadLine());
+        if (userInteger >= 17)
+        {
+            userInteger = Convert.ToInt32(Console.Read());
+            Console.WriteLine("You can see an R movie alone.");
+        }
+        else if (userInteger >= 13)
+        {
+            userInteger = Convert.ToInt32(Console.Read());
+            Console.WriteLine("You can see a PG-13 movie alone.");
+        }
+        else if (userInteger >= 5)
+        {
+            userInteger = Convert.ToInt32(Console.Read());
+            Console.WriteLine("You can see a G or PG movie alone.");
+        }
+        else
+        {
+            Console.WriteLine("Uh, you're too young for most things..");
+        }
+    }
 }
